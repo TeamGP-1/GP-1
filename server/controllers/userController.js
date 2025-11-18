d6fb68b17301ed3be81fd0a36193b3dbe8e75071
HozFisen <hozfisen@gmail.com>
2025-11-18 15:25:09 +0700

    feat: user register

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,16 @@
+const {User} = require('../models')
+
 class UserController {
     static async register(req, res, next) {
         try {
-            
+            const { email, username, password } = req.body
+            const newUser = await User.create({ email, username, password })
+
+            res.status(201).json({
+                id: newUser.id,
+                username: newUser.username,
+                email: newUser.email
+            })
         } catch (error) {
             next(error)
         }
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const { hashPassword } = require('../helpers/bcrypt');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -43,6 +44,9 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+  });
+  User.beforeCreate((user) => {
+    user.password = hashPassword(user.password)
   });
   return User;
 };
