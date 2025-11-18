6157f3f8dfa9407ecf7ec5bedaef019fe1f978a0
HozFisen <hozfisen@gmail.com>
2025-11-18 15:29:30 +0700

    feat: register

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,9 +18,31 @@ class UserController {
 
     static async login(req, res, next) {
         try {
-            
+            const { email, password } = req.body;
+
+            if (!email) throw { name: "NoEmail" };
+            if (!password) throw { name: "NoPassword" };
+
+            const user = await User.findOne({
+                where: { email }
+            });
+
+            if (!user) throw { name: "LoginError" };
+
+            if (!compare(password, user.password)) {
+                throw { name: "LoginError" }
+            };
+
+            const payload = { // Data to be stored
+                id: user.id,
+                email: user.email
+            };
+
+            const access_token = signToken(payload);
+
+            res.status(200).json({ access_token });
         } catch (error) {
-            next(error)
+            next(error);
         }
     }
 
