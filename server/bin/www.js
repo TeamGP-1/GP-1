cfbdb84bf509221937408911b94638bb5b4089fe
Nathanaells <nathanaelsinaga27@gmail.com>
2025-11-19 20:03:10 +0700

    deployment configure

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,5 +1,5 @@
 const { server } = require("../app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Listening  http://localhost:${port}`);
